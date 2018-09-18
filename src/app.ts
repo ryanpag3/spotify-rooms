c2ebfc1775ddc08439032f51b992ee7786b2d349
Ryan Page <ryanapage@gmail.com>
2018-09-18 08:58:06 -0700

    add hello world to test validity

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import env from "./utils/env";
 import conf from "../config.json";
+import homeRoute from './routes/home';
 
 const app = express();
 const config = conf[env];
 
 app.set('env', env);
 app.set('port', config.port);
+
+app.use('/', homeRoute);
 
 export default app;  
--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,5 +1,8 @@
 import Router from 'express';
+import * as homeController from '../controllers/home';
 
 const router = Router();
 
-router.get('/')
+router.get('/', homeController.index);
+
+export default router;
