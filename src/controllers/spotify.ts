648ad6724c75454f533567e82d7bdab0e8d6c020
Ryan Page <ryanapage@gmail.com>
2018-09-19 21:57:10 -0700

    first pass on authorization flow

--- src/controllers/spotify.ts
+++ src/controllers/spotify.ts
@@ -1,31 +1,15 @@
 import {Request, Response} from 'express';
-import rp from 'request-promise';
 import env from '../utils/env';
 import privateConfig from '../../config-private.json';
+import Spotify from '../services/spotify';
 
 const pConfig = privateConfig[env];
 
+
 export let callback = (req: Request, res: Response) => {
-    console.log(req.query.code);
-    console.log(pConfig.spotify.redirect_uri);
-
-    const base64 = 'Basic ' + Buffer.from(pConfig.spotify.client_id + ':' + pConfig.spotify.client_secret).toString('base64');
-
-    const options = {
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'Authorization': base64
-        },
-        uri: 'https://accounts.spotify.com/api/token',
-        body: JSON.stringify({
-            grant_type: 'authorization_code',
-            code: req.query.code,
-            redirect_uri: pConfig.spotify.redirect_uri
-        })
-    }
-    rp.post(options)
-        .then((postResult) => {
-            console.log(postResult); 
-            res.sendStatus(200)
-        });
+    let spotify = new Spotify();
+    return spotify.callback(req.query.code)
+        .then(spotify.setTokens)
+        .then(spotify.getUser)
+        .then(result => res.send(result));
 }
--- src/services/spotify.ts
+++ src/services/spotify.ts
@@ -1,23 +1,36 @@
 import env from '../utils/env';
-import { Response } from 'express';
+import {
+    Response
+} from 'express';
 import url from 'url';
+import rp from 'request-promise';
 import privateConfig from '../../config-private.json';
 
 const pConfig = privateConfig[env];
 
 export default class Spotify {
+    /* static final */
     readonly CLIENT_ID: string = pConfig.spotify.client_id;
     readonly CLIENT_SECRET: string = pConfig.spotify.client_secret;
     readonly REDIRECT_URI: string = pConfig.spotify.redirect_uri;
+
+    /* spotify URIs */
+    readonly AUTHORIZE_URI: string = 'https://accounts.spotify.com/authorize';
+    readonly CALLBACK_URI: string = 'https://accounts.spotify.com/api/token';
+    readonly GETUSER_URI: string = 'https://api.spotify.com/v1/me';
+
+    /* non-static */
+    accessToken: string;
     refreshToken: string;
 
-    constructor(refreshToken: string = undefined) {
+    constructor(refreshToken: string = undefined, accessToken: string = undefined) {
         this.refreshToken = refreshToken;
+        this.accessToken = accessToken;
     }
 
-    authorize(res: Response) {
+    authorize = (res: Response) => {
         const options = {
-            pathname: 'https://accounts.spotify.com/authorize',
+            pathname: this.AUTHORIZE_URI,
             query: {
                 response_type: 'code',
                 client_id: this.CLIENT_ID,
@@ -28,7 +41,62 @@ export default class Spotify {
         res.redirect(url.format(options));
     }
 
-    callback() {
-        
+    getClientAuth = () => {
+        return {
+            'Authorization': 'Basic ' + Buffer.from(pConfig.spotify.client_id + ':' + pConfig.spotify.client_secret).toString('base64')
+        }
+    }
+
+    getBearerAuth = () => {
+        return {
+            'Authorization': 'Bearer ' + this.accessToken
+        }
+    }
+
+    setTokens = (tokenResponse) => {
+        console.log(tokenResponse);
+        this.accessToken = tokenResponse.access_token;
+        this.refreshToken = tokenResponse.refresh_token;
+        return;
+        // return new Promise((resolve, reject) => resolve);
+    }
+
+    /**
+     * handle callback token request
+     * @param authCode - authorization code
+     * @returns access/refresh tokens, token type, token expiry, application scope
+     */
+    callback = (authCode: string) => {
+        const options = {
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+                ...this.getClientAuth()
+            },
+            uri: this.CALLBACK_URI,
+            form: {
+                grant_type: 'authorization_code',
+                code: authCode,
+                redirect_uri: pConfig.spotify.redirect_uri
+            },
+            json: true
+        }
+        return rp.post(options)
+            .then((result) => {
+                console.log(result);
+                return result;
+            });
+    }
+
+    /**
+     * 
+     */
+    getUser = () => {
+        const options = {
+            uri: this.GETUSER_URI,
+            headers: {
+                ...this.getBearerAuth()
+            }
+        }
+        return rp.get(options);
     }
 }
